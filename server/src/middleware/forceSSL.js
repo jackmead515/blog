c5dd695e8b4fae26b4d6cd6e490583a5a536d523
Jack <jack.mead@foreveroceans.com>
2022-07-17 17:22:44 -1000

    forcessl

--- server/src/middleware/forceSSL.js
+++ server/src/middleware/forceSSL.js
@@ -2,7 +2,7 @@ const config = require('../config');
 
 module.exports = function(req, res, next) {
   if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(301, `${config.domain}${req.url}`);
+    return res.redirect(301, `${config.DOMAIN}${req.url}`);
   }
 
   return next();
